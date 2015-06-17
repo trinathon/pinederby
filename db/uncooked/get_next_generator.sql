33f4ccf8746d0e23e9dcdd03bae2834582bc55f6
Richard Bowns <rbowns@insidesales.com>
2015-06-17 09:28:22 -0600

    Inverted rounds and absolute value getting generator ranges.

---
+++ db/uncooked/get_next_generator.sql
@@ -0,0 +1,42 @@
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_next_generator`(`derby` INT)
+	RETURNS int(11)
+	LANGUAGE SQL
+	NOT DETERMINISTIC
+	CONTAINS SQL
+	SQL SECURITY DEFINER
+	COMMENT 'Gets a random available generator'
+BEGIN
+# Get stdev of gen ranks
+	DECLARE next_gen,lane_count,racer_count,range_full_low,range_full_high,range_mid_low,range_mid_high INT(11);
+	DECLARE rank_avg,range_gap,range_idx,rank_stdev FLOAT(7,4);
+
+	SELECT d.number_of_lanes INTO lane_count FROM derbys AS d WHERE d.derby_id=derby;
+	SELECT COUNT(*) INTO racer_count FROM racers AS r WHERE r.derby_id = derby;
+	
+	IF NOT (SELECT COUNT(*) FROM generators AS g WHERE g.number_of_lanes = lane_count AND g.number_of_racers = racer_count) THEN
+	 CALL gen_build(derby,'',1,0);
+	END IF;
+	SELECT AVG(g.rank),STD(g.rank) INTO rank_avg,rank_stdev FROM generators AS g WHERE g.number_of_lanes = lane_count AND g.number_of_racers = racer_count;
+# round only on the last step to get the range bounderies
+#	SET range_gap = ROUND(rank_avg-3*rank_stdev);
+	SET range_gap = rank_avg-3*rank_stdev;
+# force range_idx to not repeat twice in a row?	
+#	SET range_idx = ROUND(RAND()*rank_stdev);
+	SET range_idx = RAND()*rank_stdev;
+	
+	SET range_full_low = ROUND(rank_avg-ABS((range_idx-1)*range_gap));
+	SET range_mid_low = ROUND(rank_avg-ABS(range_idx*range_gap));
+	SET range_full_high = ROUND(rank_avg+ABS(range_idx*range_gap));
+	SET range_mid_high = ROUND(rank_avg+ABS((range_idx-1)*range_gap));
+
+#	INSERT INTO debug (lane_count,racer_count,range_gap,range_idx,range_full_low,range_full_high,rank_avg,rank_stdev,range_mid_low,range_mid_high) VALUES (lane_count,racer_count,range_gap,range_idx,range_full_low,range_full_high,rank_avg,rank_stdev,range_mid_low,range_mid_high);
+	
+	SELECT g.generator_id INTO next_gen FROM generators AS g
+	 WHERE (g.number_of_lanes = lane_count AND g.number_of_racers = racer_count)
+	  AND ((range_full_low < g.rank AND range_mid_low > g.rank) OR (range_mid_high < g.rank AND range_full_high > g.rank))
+	  AND g.generator_id NOT IN
+	   (SELECT r.generator_id FROM rounds AS r WHERE r.derby_id = derby) 
+	ORDER BY RAND() LIMIT 1;
+
+	RETURN next_gen;
+END
--- db/uncooked/round_build.sql
+++ db/uncooked/round_build.sql
@@ -8,10 +8,10 @@ BEGIN
 #
 ### Get a generator
 ### Get list of racers
-# loop
-#	assign racers to lanes using generator offsets
-#	insert heat and score objects
-# insert round object
+### insert round object
+### loop
+###	assign racers to lanes using generator offsets
+###	insert heat and score objects
 # repeat for inverted round
 #
 	DECLARE round_gen,lane_count,racer_count,this_round,this_heat INT;
@@ -21,7 +21,7 @@ BEGIN
 	SET round_gen = get_next_generator(derby);
 	SELECT g.offsets INTO offsets FROM generators AS g WHERE g.generator_id = round_gen;
 	DROP TEMPORARY TABLE IF EXISTS racerIds;
-	CREATE TEMPORARY TABLE IF NOT EXISTS racerIds (row_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT) (SELECT r.racer_id FROM racers r WHERE r.derby_id = derby AND r.active = 1 ORDER BY r.last_name);
+	CREATE TEMPORARY TABLE IF NOT EXISTS racerIds (row_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT) (SELECT r.racer_id,r.last_name FROM racers r WHERE r.derby_id = derby AND r.active = 1 ORDER BY r.last_name ASC);
 	SELECT COUNT(*) INTO racer_count FROM racerIds;
 
 	INSERT INTO rounds (derby_id,generator_id) VALUES (derby,round_gen);
@@ -39,16 +39,13 @@ BEGIN
 #		handle wrap around when offset+lane1 > racer_count
 #	insert heat in heats and heat/lane/racer in scores
 		
-#		SELECT racer_id FROM racerIds WHERE row_id =i;
 		INSERT INTO heats (round_id) VALUES (this_round);
 		SELECT LAST_INSERT_ID() INTO this_heat;
 		
 		INSERT INTO scores (heat_id,racer_id,lane) VALUES (this_heat,(SELECT racer_id FROM racerIds WHERE row_id = p_idx_wrap(i,0,racer_count)),1);
 		
-#		,this_round,j,(SELECT racer_id FROM racerIds WHERE row_id = p_idx_wrap(i,0,racer_count)),offsets);
-
-		INSERT INTO debug (intgr1,intgr2,intgr3,intgr4,string1)
-		 VALUES (racer_count,this_round,j,(SELECT racer_id FROM racerIds WHERE row_id = p_idx_wrap(i,0,racer_count)),offsets);
+#		INSERT INTO debug (intgr1,intgr2,intgr3,intgr4,string1)
+#		 VALUES (racer_count,this_round,j,(SELECT racer_id FROM racerIds WHERE row_id = p_idx_wrap(i,0,racer_count)),offsets);
 
 		SET j=0;
 		vectors: LOOP
@@ -58,8 +55,62 @@ BEGIN
 			SET v = p_extract_offset(offsets,j,lane_count-1);
 
 			INSERT INTO scores (heat_id,racer_id,lane) VALUES (this_heat,(SELECT racer_id FROM racerIds WHERE row_id = p_idx_wrap(i,v,racer_count)),j+1);
-		INSERT INTO debug (intgr1,intgr2,intgr3,intgr4,intgr5,string1)
-		 VALUES (racer_count,this_round,j,v,(SELECT racer_id FROM racerIds WHERE row_id = p_idx_wrap(i,v,racer_count)),offsets);
+			
+#		INSERT INTO debug (intgr1,intgr2,intgr3,intgr4,intgr5,string1)
+#		 VALUES (racer_count,this_round,j,v,(SELECT racer_id FROM racerIds WHERE row_id = p_idx_wrap(i,v,racer_count)),offsets);
+
+			IF j < lane_count-1 THEN
+				ITERATE vectors;
+			ELSE
+				LEAVE vectors;
+			END IF;
+		END;
+		END LOOP vectors;
+ 		
+		IF i < racer_count THEN
+			ITERATE lanes;
+		ELSE
+			LEAVE lanes;
+		END IF;
+	END;
+	END LOOP lanes;
+	END;
+# inverted
+#ALTER
+	DROP TEMPORARY TABLE IF EXISTS invRacerIds;
+	CREATE TEMPORARY TABLE IF NOT EXISTS invRacerIds (row_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT) (SELECT r.racer_id FROM racers r WHERE r.derby_id = derby AND r.active = 1 ORDER BY r.last_name DESC);
+#del	SELECT COUNT(*) INTO racer_count FROM racerIds;
+
+	INSERT INTO rounds (derby_id,generator_id) VALUES (derby,0-round_gen);
+	SELECT LAST_INSERT_ID() INTO this_round;
+	
+	BEGIN
+	DECLARE i INT;
+	SET i=0;
+	lanes: LOOP
+	BEGIN
+		DECLARE j INT;
+		SET i = i+1;
+		
+		INSERT INTO heats (round_id) VALUES (this_round);
+		SELECT LAST_INSERT_ID() INTO this_heat;
+		
+		INSERT INTO scores (heat_id,racer_id,lane) VALUES (this_heat,(SELECT racer_id FROM invRacerIds WHERE row_id = p_idx_wrap(i,0,racer_count)),1);
+		
+#		INSERT INTO debug (intgr1,intgr2,intgr3,intgr4,string1)
+#		 VALUES (racer_count,this_round,j,(SELECT racer_id FROM racerIds WHERE row_id = p_idx_wrap(i,0,racer_count)),offsets);
+
+		SET j=0;
+		vectors: LOOP
+		BEGIN
+			DECLARE v INT;
+			SET j=j+1;
+			SET v = p_extract_offset(offsets,j,lane_count-1);
+
+			INSERT INTO scores (heat_id,racer_id,lane) VALUES (this_heat,(SELECT racer_id FROM invRacerIds WHERE row_id = p_idx_wrap(i,v,racer_count)),j+1);
+			
+#		INSERT INTO debug (intgr1,intgr2,intgr3,intgr4,intgr5,string1)
+#		 VALUES (racer_count,this_round,j,v,(SELECT racer_id FROM racerIds WHERE row_id = p_idx_wrap(i,v,racer_count)),offsets);
 
 			IF j < lane_count-1 THEN
 				ITERATE vectors;
